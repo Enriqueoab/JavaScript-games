df14865af747a7dfd785c87c87d53b868c872736
Enrique Barca <barcaenrique7@gmail.com>
2021-07-05 20:28:22 +0100

    Board, listeners, grid html basic done

--- memory-game/app.js
+++ memory-game/app.js
@@ -1,4 +1,85 @@
-<<<<<<< HEAD
-=======
+document.addEventListener('DOMContentLoaded',() =>{
 
->>>>>>> bfc5f0abf979f6ed7deba39fea0a5bae5295180d
+//Array of cards with name to call them later    
+const cardArray =[
+{
+    name:'tesla',
+    img:'images/tesla.jpg'
+},
+{
+    name:'tesla',
+    img:'images/tesla.jpg'
+
+},
+{
+    name:'nissan',
+    img:'images/nissan.jpg'
+
+},
+{
+    name:'nissan',
+    img:'images/nissan.jpg'
+
+},
+{
+    name:'acura',
+    img:'images/acura.jpg'
+
+},
+{
+    name:'acura',
+    img:'images/acura.jpg'
+
+},
+{
+    name:'lotus',
+    img:'images/lotus.jpg'
+
+},
+{
+    name:'lotus',
+    img:'images/lotus.jpg'
+
+},
+{
+    name:'bugati',
+    img:'images/bugati.jpg'
+
+},
+{
+    name:'bugati',
+    img:'images/bugati.jpg'
+
+},
+{
+    name:'audi',
+    img:'images/audi.jpg'
+
+},
+{
+    name:'audi',
+    img:'images/audi.jpg'
+
+}
+]//End of the array
+
+//Pick up the class element "grid" from our html
+const grid =document.querySelector('.grid')
+
+//Start of the game-board
+function createBoard(){
+
+//We loop our array to create a img element
+for (let i = 0; i < cardArray.length; i++) {
+    var card= document.createElement('img')
+    card.setAttribute('src','images/cover.jpg')
+    card.setAttribute('id',i)//Set an id to each card
+    //Listening Img element clicked
+    //card.addEventListener('click',cardFliped());
+    //To put our img element created into de div (grid) 
+    grid.appendChild(card)
+}
+}
+createBoard()
+
+})//End of the listener event
