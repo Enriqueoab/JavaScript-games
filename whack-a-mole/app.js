b7686b4cc73d2b15101141132c88b78f61211b81
Enrique Barca <barcaenrique7@gmail.com>
2021-07-06 20:27:30 +0100

    Concepts, getRandomSquare function and mouse listener

---
+++ whack-a-mole/app.js
@@ -0,0 +1,60 @@
+//-----------Concepts-------------------------
+/*classList, This property is useful to add 
+EX: (square.classList(mole) = square mole),
+ remove and toggle CSS classes on an element.
+
+Arrow function(=>),These are for shorter function syntax
+*/
+
+const squares=document.querySelectorAll('.square');
+const mole =document.querySelectorAll('.mole');
+const timeLeft=document.querySelector('#time-left');
+
+let score=document.querySelector('#score');
+let result=0;
+
+function getRandomSquare(){
+    
+    /*For each "square" class name that the collection "squares" has
+    we remove the one with mole in it*/
+    squares.forEach( className => {
+        //Removing the class 'mole' of our grid
+        className.classList.remove('mole')
+       
+    });
+        //Math.floor to round down the integer
+    let randomSquare= squares[Math.floor(Math.random() * 9)];
+    
+    //Now we add to the random position of our grid
+    //the name class mole (That way our css file will
+    //set the image)
+    randomSquare.classList.add('mole');
+
+    //randomSquare is a square element of our html
+    //so we can get the id where the user should click
+    hitPosition=randomSquare.id;
+
+} 
+
+//Using the id of our square div we set the listener to them
+//and compare to know if the user click on it
+squares.forEach(id =>{
+    id.addEventListener('mouseup',() =>{
+
+        if (id.id === hitPosition) {
+            
+            result=result + 1;
+            score.textContent=result;
+
+        } 
+    })
+})
+
+function moleMovement(){
+
+let molePosition=null;
+molePosition= setInterval(getRandomSquare,300);
+
+}
+//time video 18:30
+moleMovement();
